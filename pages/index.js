b170e0d5073b8281717ae318e02d3a8cc5000566
VICTOR MERSEGUEL <merseguel@gmail.com>
2023-08-23 12:25:29 +0100

    change phrase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Luciana, eu te amo. Se você também me ama, dá uma risadinha!</h1>
+    return <h1>Luciana, eu te amo. Se você também me ama, dá uma risadinha! 😎</h1>
 }
 
 export default Home;
