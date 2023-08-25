6f8d4516ed714e5100a362281ac759772b8515bb
VICTOR MERSEGUEL <merseguel@gmail.com>
2023-08-25 12:58:23 +0100

    add `lint:check` and `lint:fix` scripts

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 function Home() {
-    return <h1>Luciana, eu te amo. Se você também me ama, dá uma risadinha! 😎</h1>
+  return (
+    <h1>Luciana, eu te amo. Se você também me ama, dá uma risadinha! 😎</h1>
+  );
 }
 
 export default Home;
